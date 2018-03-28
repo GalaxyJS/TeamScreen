ea78664cc4956c171a498ac96b9fece1b6d5189b
Petri <petri.van.niekerk@hva.nl>
2018-03-28 15:37:00 +0200

    Added Dominic js Frame Controller

--- widgets/WidgetFrameControl.js
+++ widgets/WidgetFrameControl.js
@@ -1,35 +1,18 @@
+/* @author Dominic */
+const FRAMES_PER_SECOND = 20;
+const FRAME_PAUSE_TIME_IN_MS = 5000;
 
-
-/**
- * Animate the list if the list is longer than the defined widget height.
- * @type {HTMLElement | null}
- */
-var drinklist = document.getElementById('drink-list');
-var FRAMES_PER_SECOND = 20;
-var FRAME_PAUSE_TIME_IN_MS = 5000;
-
-/**
- * Pauses widget frame when it reaches bottom and top of the frame.
- * @param milliseconds
- */
-function sleep(milliseconds) {
-    var currentTime = new Date().getTime();
-    while (currentTime + milliseconds >= new Date().getTime()) {
-    }
-}
-function step() {
-    console.log(drinklist.scrollTop + 2);
-    console.log((drinklist.scrollHeight - drinklist.offsetHeight));
-    if (drinklist.scrollTop <= 0) {
-        direction = 1;
-        sleep(FRAME_PAUSE_TIME_IN_MS);
-    } else if (drinklist.scrollTop + 2 > ((drinklist.scrollHeight - drinklist.offsetHeight))) {
-        direction = -1;
-        sleep(FRAME_PAUSE_TIME_IN_MS);
-    }
-    drinklist.scrollTop += direction;
-    setTimeout(function () {
-        window.requestAnimationFrame(step);
-    }, 1000 / FRAMES_PER_SECOND);
-}
-window.requestAnimationFrame(step);
+$(document).ready(function(){
+    // For each scrollable
+    $('.scrollable').each(function(){
+        // Cache this
+        $this = $(this);
+        // Calculate scroll distance
+        $this.scrollDistance = $this[0].scrollHeight - $this.height();
+        // Set timer
+        window.setInterval(function(){
+            var target = $this.scrollTop() == 0 ? $this.scrollDistance : 0;
+            $this.animate({scrollTop : target}, 1000);
+        }, FRAME_PAUSE_TIME_IN_MS);
+    });
+});
