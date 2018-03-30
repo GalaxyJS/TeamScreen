d94dbcf5b9eaaf819753022e87b5f53c6e888286
Unknown <dominic@we-design.eu>
2018-03-30 11:18:54 +0200

    changed draw loop

--- widgets/WidgetFrameControl.js
+++ widgets/WidgetFrameControl.js
@@ -4,21 +4,30 @@ const FRAME_PAUSE_TIME_IN_MS = 5000;
 const ANIMATION_SPEED = parseInt(0.5 * FRAME_PAUSE_TIME_IN_MS);
 
 $(document).ready(function(){
-    // For each scrollable
-    $('.scrollable').each(function(){
+    // Cache scrollables
+    var $scrollables = $('.scrollable');
+
+    // Caculate scrolling distances
+    $scrollables.each(function() {
         // Cache variables
         var $this = $(this);
         var scrollHeight = $this[0].scrollHeight;
         // Return if scrolling is not needed.
         if (scrollHeight <= $this.height()) return;
         // Calculate scroll distance
-        $this.scrollDistance = scrollHeight - $this.height();
-        // Set timer
-        window.setInterval(function(){
-            var target = $this.scrollTop() == 0 ? $this.scrollDistance : 0;
-
-
-            $this.animate({scrollTop : target}, ANIMATION_SPEED);
-        }, FRAME_PAUSE_TIME_IN_MS);
+        $this.data('scrollDistance', scrollHeight - $this.height());
     });
+
+    function draw(){
+        $scrollables.each(function(){
+            var $this = $(this);
+            var target = $this.scrollTop() == 0 ? $this.data('scrollDistance') : 0;
+            $this.animate({scrollTop : target}, ANIMATION_SPEED);
+        });
+    }
+
+    // Set loop
+    window.setInterval(function(){
+        draw();
+    }, FRAME_PAUSE_TIME_IN_MS);
 });
